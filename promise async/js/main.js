0f71644d806549917814bf251964445a6c193074
Burdiugh <burdiugh.bk@gmail.com>
2022-06-27 17:12:35 +0300

    promise swapi is done

--- promise async/js/main.js
+++ promise async/js/main.js
@@ -2,27 +2,61 @@ const URL_PEOPLE = "https://swapi.dev/api/people";
 const URL_PLANET = "https://swapi.dev/api/planets";
 const URL_STARSHIPS = "https://swapi.dev/api/starships";
 
+let currentObject;
+let currentSelect = "people";
+
 window.addEventListener("load", Init);
+
 
 function Init(){
 
-  Request(URL_PEOPLE, PrintPeson);
+  Request(URL_PEOPLE, PrintPerson);
   const people = document.querySelector(".people");
   const planets = document.querySelector(".planets");
   const starships = document.querySelector(".starships");
 
   people.addEventListener('click', () => {
-    Request(URL_PEOPLE, PrintPeson);
+    currentSelect = "people";
+    Request(URL_PEOPLE, PrintPerson);
   })
 
   planets.addEventListener('click', () => {
+    currentSelect = "planets";
     Request(URL_PLANET, PrintPlanets);
   })
 
   starships.addEventListener('click', () => {
+    currentSelect = "starships";
     Request(URL_STARSHIPS, PrintStarships);
   })
 
+}
+
+const nextBtn = document.querySelector(".nextBtn");
+
+  nextBtn.addEventListener("click", () => {
+    Next(currentObject.next, GetCurrentPrintCallback());
+  });
+
+  const previousBtn = document.querySelector(".previousBtn");
+  previousBtn.addEventListener("click", () => {
+    Next(currentObject.previous, GetCurrentPrintCallback());
+  });
+
+
+function GetCurrentPrintCallback() {
+  switch (currentSelect) {
+    case "people":
+      return PrintPerson;
+    case "planets":
+      return PrintPlanets;
+    case "starships":
+      return PrintStarships;
+  }
+}
+
+function Next(URL, Callback) {
+  Request(URL, Callback);
 }
 
 function Request(URL, Callback){
@@ -30,101 +64,60 @@ function Request(URL, Callback){
   .then(result => {
     return result.json();
   }).then(data => {
+    console.log(data);
+    const nextBtn = document.querySelector(".nextBtn");
+    if (data.next == null) {
+      
+      nextBtn.setAttribute("disabled","disabled");
+    } else {
+      
+      nextBtn.removeAttribute("disabled","disabled");
+    }
+    const previousBtn = document.querySelector(".previousBtn");
+    if (data.previous == null) {
+      previousBtn.setAttribute("disabled", "disabled");
+    } else {
+      previousBtn.removeAttribute("disabled","disabled");
+    }
+    currentObject = data;
+
     Callback(data);
-  })
+  }) 
   .catch(err => console.log(err))
 }
 
-PrintPeson = ({results}) => {
-    const body = document.querySelector(".data-block");
-  body.querySelectorAll("*").forEach((n) => n.remove());
-    results.forEach((element)=>{
-
-        const data2 = document.querySelector(".data-block");
-        const info = document.createElement("div");
-
-        info.setAttribute('class', 'element_info');
-
-        const element_name = document.createElement('h1');
-        element_name.innerHTML = element.name;
-
-        const element_height = document.createElement("h3");
-        element_height.innerHTML = "Height: " + element.height;
-
-    const element_mass = document.createElement("h3");
-    element_mass.innerHTML = "Mass: " + element.mass;
-
-    const element_Gender = document.createElement("h3");
-    element_Gender.innerHTML = "Gender: " + element.gender;
-
-        info.appendChild(element_name);
-        info.appendChild(element_height);
-        info.appendChild(element_mass);
-        info.appendChild(element_Gender);
-        data2.appendChild(info);
-
-    })
-
-    
-  //console.log(persons)
+PrintPerson = ({results}) => {
+Processing(results);
 };
-
 
 PrintPlanets = ({results}) => {
-    const body = document.querySelector(".data-block");
-  body.querySelectorAll("*").forEach((n) => n.remove());
-    results.forEach((element)=>{
-
-        const data2 = document.querySelector(".data-block");
-        const info = document.createElement("div");
-
-        info.setAttribute('class', 'element_info');
-
-        const element_name = document.createElement('h1');
-        element_name.innerHTML = element.name;
-
-        const element_diameter = document.createElement("h3");
-        element_diameter.innerHTML = "Diameter: " + element.diameter;
-
-    const element_climate = document.createElement("h3");
-    element_climate.innerHTML = "Climate: " + element.climate;
-
-
-        info.appendChild(element_name);
-        info.appendChild(element_diameter);
-        info.appendChild(element_climate);
-        data2.appendChild(info);
-
-    })
-
-    
-  //console.log(persons)
+  Processing(results);
 };
 
-  PrintStarships = ({ results }) => {
-    const body = document.querySelector(".data-block");
-  body.querySelectorAll("*").forEach((n) => n.remove());
-    results.forEach((element) => {
-      const data2 = document.querySelector(".data-block");
-      const info = document.createElement("div");
-      info.setAttribute("class", "element_info");
-  
-      const element_name = document.createElement("h1");
-      element_name.innerHTML = element.name;
-  
-      const element_model = document.createElement("h3");
-      element_model.innerHTML = "Model: " + element.model;
-  
-      const element_manufacturer = document.createElement("h3");
-      element_manufacturer.innerHTML = "Manufacturer: " + element.manufacturer;
-  
-      const element_length = document.createElement("h3");
-      element_length.innerHTML = "Length: " + element.length;
-  
-      info.appendChild(element_name);
-      info.appendChild(element_model);
-      info.appendChild(element_manufacturer);
-      info.appendChild(element_length);
-      data2.appendChild(info);
-    });
-  };
+PrintStarships = ({results}) => {
+     Processing(results);
+};
+
+function Processing(arr){
+  RemoveElements();
+
+
+  const data = document.querySelector(".data-block");
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "bodyWrapper");
+  data.appendChild(wrapper);
+
+  arr.forEach((item) => {
+    console.log(item.name);
+
+    const pName = document.createElement("p");
+    pName.textContent = item.name;
+    pName.setAttribute("class", "bodyElement");
+    wrapper.appendChild(pName);
+  });
+}
+
+  function RemoveElements() {
+    let element = document.querySelector(".bodyWrapper");
+    element.remove();
+  }
